6782d24ac68f28218c70a4270dd63f6234534cc3
amharrison <amharrison@gmail.com>
2021-03-22 14:55:00 -0400

    - update for unit 7, reduced probe interval

--- org.jactr.tutorial.unit4/src/org/jactr/tutorial/unit4/paired/chunk/ChunkNamer.java
+++ org.jactr.tutorial.unit4/src/org/jactr/tutorial/unit4/paired/chunk/ChunkNamer.java
@@ -4,10 +4,10 @@ import org.jactr.core.chunk.IChunk;
 import org.jactr.core.chunk.ISymbolicChunk;
 import org.jactr.core.module.declarative.basic.chunk.IChunkNamer;
 
-
 /**
  * This little snippet is useful for creating custom chunk names for your model.
  * This is applied to all chunks, so never return null.
+ * 
  * @author harrison
  *
  */
@@ -17,14 +17,17 @@ public class ChunkNamer implements IChunkNamer {
 	public String generateName(IChunk chunk) {
 		ISymbolicChunk sc = chunk.getSymbolicChunk();
 		if (sc.getChunkType().getSymbolicChunkType().getName().equals("pair")) {
-			//for paired associates
+			// for paired associates
 			return String.format("pair-%s-%s", sc.getSlot("probe").getValue(), sc.getSlot("answer").getValue());
-		} else if(sc.getChunkType().getSymbolicChunkType().getName().equals("problem")) {
-			//for zbrodoff
+		} else if (sc.getChunkType().getSymbolicChunkType().getName().equals("problem")) {
+			// for zbrodoff
 			return String.format("problem-%s%s%s", sc.getSlot("arg1").getValue(), sc.getSlot("arg2").getValue(),
 					sc.getSlot("result").getValue());
-					
-		}else
+
+		} else if (sc.getChunkType().getSymbolicChunkType().getName().equals("args")) {
+			// for unit 7
+			return String.format("pair-%s%s", sc.getSlot("arg1").getValue(), sc.getSlot("arg2").getValue());
+		} else
 			return sc.getName();
 	}
 
